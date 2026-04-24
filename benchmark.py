2564a85b8c0bbd65a00d91411334a227789a730b
ash1ra <ash1rawtf@gmail.com>
2026-04-24 14:44:29 +0300

    feat: adopt `compare` and `upscale` methods to work with multiple images and configs

--- benchmark.py
+++ benchmark.py
@@ -7,7 +7,7 @@ import yaml
 from torch.nn import functional as F
 
 from evaluator import Evaluator
-from utils import imresize_tensor
+from core_utils import imresize
 from visualizer import Visualizer
 
 
@@ -68,81 +68,97 @@ class Benchmark:
 
     def upscale(
         self,
-        config_path: Path,
-        input_img_path: Path,
-        output_img_path: Path,
+        config_paths: list[Path],
+        input_img_paths: list[Path],
+        output_img_paths: list[Path],
         downscale: bool = False,
     ) -> None:
-        img_tensor = self.visualizer.read_image(input_img_path)
-
-        evaluator = self._init_evaluator(config_path)
-
-        if downscale:
-            sr_img_tensor = evaluator.upscale_downscaled(img_tensor)
-        else:
-            sr_img_tensor = evaluator.upscale(img_tensor)
-
-        self.visualizer.save_image(sr_img_tensor, output_img_path)
-
-        del evaluator
-        gc.collect()
-        torch.cuda.empty_cache()
-
-    def compare(
-        self,
-        config_paths: list[Path],
-        hr_img_path: Path,
-        output_img_path: Path,
-        lr_img_path: Path | None = None,
-        crop_size: int | None = None,
-    ) -> None:
-        hr_img_tensor = self.visualizer.read_image(hr_img_path)
-
-        _, hr_img_height, hr_img_width = hr_img_tensor.shape
-
-        if not lr_img_path:
-            with open(config_paths[0], "r", encoding="UTF-8") as f:
-                scaling_factor = yaml.safe_load(f)["model_params"]["scaling_factor"]
-
-            lr_img_tensor = imresize_tensor(hr_img_tensor, scale=1 / scaling_factor, antialiasing=True)
-        else:
-            lr_img_tensor = self.visualizer.read_image(lr_img_path)
-
-        results = {
-            "Bicubic": F.interpolate(
-                lr_img_tensor.unsqueeze(0),
-                size=(hr_img_height, hr_img_width),
-                mode="bicubic",
-            )
-            .squeeze(0)
-            .clamp(0, 1)
-        }
-
         for config_path in config_paths:
             evaluator = self._init_evaluator(config_path)
-            results[config_path.parent.name] = evaluator.upscale(lr_img_tensor)
+
+            for input_img_path, output_img_path in zip(input_img_paths, output_img_paths):
+                img_tensor = self.visualizer.read_image(input_img_path)
+
+                if downscale:
+                    sr_img_tensor = evaluator.upscale_downscaled(img_tensor)
+                else:
+                    sr_img_tensor = evaluator.upscale(img_tensor)
+
+                output_img_path = output_img_path.parent / f"{evaluator.model_name.lower()}_{output_img_path.name}.png"
+
+                self.visualizer.save_image(sr_img_tensor, output_img_path)
 
             del evaluator
             gc.collect()
             torch.cuda.empty_cache()
 
-        results["Original (HR)"] = hr_img_tensor
+    def compare(
+        self,
+        config_paths: list[Path],
+        hr_img_paths: list[Path],
+        output_img_paths: list[Path],
+        lr_img_paths: list[Path | None] | None = None,
+        crop_size: int | None = None,
+    ) -> None:
+        hr_img_tensors = [self.visualizer.read_image(path) for path in hr_img_paths]
+        lr_img_tensors = []
 
-        if crop_size:
-            crop_center_coords = self.visualizer.get_crop_center(img_tensor=hr_img_tensor, crop_size=crop_size)
+        with open(config_paths[0], "r", encoding="UTF-8") as f:
+            scaling_factor = yaml.safe_load(f)["model_params"]["scaling_factor"]
 
-            self.visualizer.create_collage_with_crop(
-                hr_img_tensor=hr_img_tensor,
-                sr_img_tensors_dict=results,
-                crop_center=crop_center_coords,
-                crop_size=crop_size,
-                output_img_path=output_img_path,
+        for i, hr_img_tensor in enumerate(hr_img_tensors):
+            lr_img_path = lr_img_paths[i] if lr_img_paths else None
+            if lr_img_path is None:
+                lr_img_tensor = imresize(hr_img_tensor, scaling_factor=1 / scaling_factor, antialiasing=True)
+                lr_img_tensors.append(lr_img_tensor)
+            else:
+                lr_img_tensor = self.visualizer.read_image(lr_img_path)
+                lr_img_tensors.append(lr_img_tensor)
+
+        results = []
+        for hr_img_tensor, lr_img_tensor in zip(hr_img_tensors, lr_img_tensors):
+            _, hr_img_height, hr_img_width = hr_img_tensor.shape
+            results.append(
+                {
+                    "Bicubic": F.interpolate(
+                        lr_img_tensor.unsqueeze(0),
+                        size=(hr_img_height, hr_img_width),
+                        mode="bicubic",
+                    )
+                    .squeeze(0)
+                    .clamp(0, 1)
+                }
             )
-        else:
-            self.visualizer.create_collage(
-                sr_img_tensors_dict=results,
-                output_img_path=output_img_path,
-            )
+
+        for config_path in config_paths:
+            evaluator = self._init_evaluator(config_path)
+            model_name = config_path.parent.name
+
+            for i, lr_img_tensor in enumerate(lr_img_tensors):
+                results[i][model_name] = evaluator.upscale(lr_img_tensor)
+
+            del evaluator
+            gc.collect()
+            torch.cuda.empty_cache()
+
+        for i, hr_img_tensor in enumerate(hr_img_tensors):
+            results[i]["Original (HR)"] = hr_img_tensor
+
+            if crop_size:
+                crop_center_coords = self.visualizer.get_crop_center(img_tensor=hr_img_tensor, crop_size=crop_size)
+
+                self.visualizer.create_collage_with_crop(
+                    hr_img_tensor=hr_img_tensor,
+                    sr_img_tensors_dict=results[i],
+                    crop_center=crop_center_coords,
+                    crop_size=crop_size,
+                    output_img_path=output_img_paths[i],
+                )
+            else:
+                self.visualizer.create_collage(
+                    sr_img_tensors_dict=results[i],
+                    output_img_path=output_img_paths[i],
+                )
 
 
 if __name__ == "__main__":
@@ -153,29 +169,30 @@ if __name__ == "__main__":
     models_to_test = list(Path("models").glob("*/config.yaml"))
     datasets = [Path("data/Set5"), Path("data/Set14")]
 
-    benchmark.evaluate(
-        config_paths=models_to_test,
-        dataset_paths=datasets,
-        save_csv_path=Path("results/final_benchmark.csv"),
-    )
+    # benchmark.evaluate(
+    #     config_paths=models_to_test,
+    #     dataset_paths=datasets,
+    #     save_csv_path=Path("results/final_benchmark.csv"),
+    # )
 
-    benchmark.upscale(
-        config_path=Path("models/SRResNet/config.yaml"),
-        input_img_path=Path("images/hr_img_1.jpg"),
-        output_img_path=Path("results/hat_hr_img_1.png"),
-        downscale=True,
-    )
+    # benchmark.upscale(
+    #     config_paths=models_to_test,
+    #     input_img_paths=[Path("images/hr_img_1.jpg")],
+    #     output_img_paths=[Path("results/sr_img_1.png")],
+    #     downscale=True,
+    # )
+
+    # benchmark.compare(
+    #     config_paths=models_to_test,
+    #     hr_img_path=Path("data/Urban100/HR/img_071.png"),
+    #     lr_img_path=Path("data/Urban100/LR_x4/img_071.png"),
+    #     output_img_path=Path("results/img_071_comparison.png"),
+    #     crop_size=64,
+    # )
 
     benchmark.compare(
         config_paths=models_to_test,
-        hr_img_path=Path("images/img_073.png"),
-        lr_img_path=Path("data/Urban100/LR_x4/img_073.png"),
-        output_img_path=Path("results/img_073_comparison.png"),
-        crop_size=64,
-    )
-
-    benchmark.compare(
-        config_paths=models_to_test,
-        hr_img_path=Path("images/hr_baboon.png"),
-        output_img_path=Path("results/baboon_comparison.png"),
+        hr_img_paths=[Path("data/Set14/HR/flowers.png"), Path("./data/Manga109/HR/HealingPlanet.png")],
+        lr_img_paths=[Path("data/Set14/LR_x4/flowers.png"), None],
+        output_img_paths=[Path("results/flowers_comparison.png"), Path("results/manga_comparison.png")],
     )
